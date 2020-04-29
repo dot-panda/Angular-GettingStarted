b0125a60d75c6050d3290df46bbca4cccf201509
dot-panda <masnek@gmail.com>
2020-04-28 22:22:29 -0500

    commit from Win10

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({ // component decorator
   selector: 'pm-root', // directive name used in html
-  templateUrl:
-
-   './app.component.html', // view template and layout
-   styleUrls: ['./app.component.css']
+  template: `
+  <div style="text-align:center">
+  <h1>
+    {{pageTitle}}
+  </h1>
+  <div>My First Component</div>
+  <pm-products></pm-products>
+  ... Starter Files ...
+  </div>
+  `
+  // './app.component.html', // view template and layout
+  // styleUrls: ['./app.component.css']
 
 })
 export class AppComponent {
